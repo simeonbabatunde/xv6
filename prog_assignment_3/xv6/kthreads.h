bc70038665ecac793e05b740793f76635bb26e9a
simeon <sbabatu@clemson.edu>
2018-02-21 22:24:34 -0500

    added testcase for stack

--- prog_assignment_3/xv6/kthreads.h
+++ prog_assignment_3/xv6/kthreads.h
@@ -6,6 +6,9 @@ typedef struct __lock_t{
 	uint flag;
 }lock_t;
 
+
+int thread_create(void (*start_routine)(void*), void *arg);
+int thread_join();
 int lock_init(lock_t *lk);
 void lock_acquire(lock_t *lk);
 void lock_release(lock_t *lk);
